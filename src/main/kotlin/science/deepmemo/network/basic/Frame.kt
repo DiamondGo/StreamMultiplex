d0d661ed96fe43768bcf7828a6e6cbea5e56b81b
Ke Xie <xie.kenneth@gmail.com>
2017-11-05 12:07:26 +0800

    add Frame

--- src/main/kotlin/science/deepmemo/network/basic/Frame.kt
+++ src/main/kotlin/science/deepmemo/network/basic/Frame.kt
@@ -1,2 +1,46 @@
 package science.deepmemo.network.basic
 
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+
+val version: Byte = 1
+
+enum class Command(val value: Byte) {
+    SYN(0),
+    FIN(1),
+    PSH(2),
+    NOP(3);
+
+    companion object {
+        private val map = Command.values().associateBy(Command::value);
+        fun fromInt(type: Byte) = map[type]
+    }
+}
+
+data class Frame(
+        val version: Byte,
+        val command: Command,
+        val streamId: Int,
+        val data: ByteArray
+)
+
+typealias RawData = ByteArray
+
+val RawData.version: Byte
+    get() = this[0]
+
+val RawData.command: Command
+    get() = Command.fromInt(this[1])!!
+
+val RawData.length: Int get() {
+    val buffer = ByteBuffer.wrap(this.sliceArray((2..3))).order(ByteOrder.LITTLE_ENDIAN)
+    return buffer.short.toInt()
+}
+
+val RawData.streamId: Int get() {
+    val buffer = ByteBuffer.wrap(this.sliceArray((4..7))).order(ByteOrder.LITTLE_ENDIAN)
+    return buffer.int
+}
+
+val RawData.data: ByteArray get() = this.sliceArray((8..(8 + this.length - 1)))
+
